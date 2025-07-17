9b4e6554d65a2ed0ebcdd00548685ecb9b3475b2
jesusyegres-13 <jyegres@boomid.io>
2025-07-17 13:21:40 -0500

    feat: Integrate Safe Area Context and update project configuration

--- GentleSteps/App.tsx
+++ GentleSteps/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import LoginScreen from './src/LoginScreen';
 
 const App = () => {
-  return <LoginScreen />;
+  return (
+    <SafeAreaProvider>
+      <LoginScreen />
+    </SafeAreaProvider>
+  );
 };
 
 export default App;
